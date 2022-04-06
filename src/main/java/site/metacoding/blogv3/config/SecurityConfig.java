0123103afef17fbb366ac387e7f1ae627a3a6478
LeeSeonHo113 <3tjsgh@naver.com>
2022-04-06 17:46:20 +0900

    ~~~ 엔티티 생성중...

--- src/main/java/site/metacoding/blogv3/config/SecurityConfig.java
+++ src/main/java/site/metacoding/blogv3/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // super.configure(http);
-        http.csrf().disable();
+        http.csrf().disable(); // 이거 안 하면 postman 테스트 못 함.
         http.authorizeRequests()
                 .antMatchers("/s/**").authenticated()
                 .anyRequest().permitAll()
