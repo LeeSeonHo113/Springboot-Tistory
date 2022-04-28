dd59354b0db86ac8250d77c6a3f7a45fb5c7c322
a01048197443 <a01048197443@gmail.com>
2022-04-28 10:51:49 +0900

    삭제 로직 수정- 1/2 미완

--- src/main/java/site/metacoding/blogv3/web/PostController.java
+++ src/main/java/site/metacoding/blogv3/web/PostController.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.config.auth.LoginUser;
 import site.metacoding.blogv3.domain.category.Category;
+import site.metacoding.blogv3.domain.post.Post;
+import site.metacoding.blogv3.domain.user.User;
 import site.metacoding.blogv3.handler.ex.CustomException;
 import site.metacoding.blogv3.service.PostService;
 import site.metacoding.blogv3.web.dto.post.PostRespDto;
@@ -28,8 +32,23 @@ public class PostController {
     // CategoryService 사용하지 말고
     // PostService 사용하세요. 이유는 나중에 category, post글 다 같이 가지고 가야 하기 때문임!!
 
-    @GetMapping("/user/{userId}/post/{postId}")
-    public String detail(Model model, @AuthenticationPrincipal LoginUser loginUser) {
+    @DeleteMapping("/s/api/post/{id}")
+    public ResponseEntity<?> postDelete(@PathVariable Integer id, @AuthenticationPrincipal LoginUser loginUser) {
+
+        User principal = loginUser.getUser();
+
+        postService.게시글삭제(id, principal);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable Integer id, Model model, @AuthenticationPrincipal LoginUser loginUser) {
+
+        Post postEntity = postService.게시글상세보기(id);
+        model.addAttribute("post", postEntity);
+
+        // Dto 내일!!
 
         return "/post/detail";
     }
@@ -54,8 +73,8 @@ public class PostController {
         return "/post/writeForm";
     }
 
-    @GetMapping("/user/{id}/post")
-    public String postList(Integer categoryId, @PathVariable Integer id,
+    @GetMapping("/user/{pageOwnerId}/post")
+    public String postList(Integer categoryId, @PathVariable Integer pageOwnerId,
             @AuthenticationPrincipal LoginUser loginUser,
             Model model,
             @PageableDefault(size = 3) Pageable pageable) {
@@ -64,30 +83,12 @@ public class PostController {
         PostRespDto postRespDto = null;
 
         if (categoryId == null) {
-            postRespDto = postService.게시글목록보기(id, pageable);
+            postRespDto = postService.게시글목록보기(pageOwnerId, pageable);
         } else {
-            postRespDto = postService.게시글카테고리별보기(id, categoryId, pageable);
+            postRespDto = postService.게시글카테고리별보기(pageOwnerId, categoryId, pageable);
         }
 
         model.addAttribute("postRespDto", postRespDto);
         return "/post/list";
-    }
-
-    @GetMapping("/test/user/{id}/post")
-    public @ResponseBody PostRespDto testPostList(Integer categoryId, @PathVariable Integer id,
-            @AuthenticationPrincipal LoginUser loginUser,
-            Model model,
-            @PageableDefault(size = 3) Pageable pageable) {
-        // SELECT * FROM category WHERE userId = :id
-        // 카테고리 가져가세요!!
-        PostRespDto postRespDto = null;
-
-        if (categoryId == null) {
-            postRespDto = postService.게시글목록보기(id, pageable);
-        } else {
-            postRespDto = postService.게시글카테고리별보기(id, categoryId, pageable);
-        }
-
-        return postRespDto;
     }
 }
